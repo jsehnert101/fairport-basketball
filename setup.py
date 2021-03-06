ce6b5ceab47cd5b705e378fac718334ef0c3d73e
jsehnert101 <jsehnert101@gmail.com>
2021-03-06 16:01:19 -0500

    getting started

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repository for Fairport Basketball analytics program',
+    author='Jake Sehnert',
+    license='BSD-3',
+)
